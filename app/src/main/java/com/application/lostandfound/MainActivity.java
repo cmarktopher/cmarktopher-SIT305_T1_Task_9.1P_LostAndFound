e730b20f797af013b366a45cd59b0467f1124aab
chris-marktopher <cmarktopher@gmail.com>
2023-04-08 14:15:46 +1000

    Added room persistence layer dependencies.

--- app/src/main/java/com/application/lostandfound/MainActivity.java
+++ app/src/main/java/com/application/lostandfound/MainActivity.java
@@ -4,6 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+/**
+ * Some notes for the app
+ * Based on https://developer.android.com/training/data-storage/sqlite,
+ * it seems that a recommended approach is to use the Room Persistence Library.
+ * As such, I am following this as well: https://developer.android.com/training/data-storage/room
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
